d71f1555dbda574aeb458b01fa3bf3b8b4ada942
Krzysztof Piątkowski <ra.krzysztof.piatkowski@gmail.com>
2020-05-19 20:55:35 +0200

    Algorytmy sortowania w jednym pliku

---
+++ Algorytmy_sortowania/Calosc/Algorithms.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calosc
+{
+    class Algorithms
+    {
+        public void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        public void QuickSort(int[] array, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            int pivot = array[(left + right) / 2];
+            while (i < j)
+            {
+                while (array[i] < pivot) i++;
+                while (array[j] > pivot) j--;
+                if (i <= j)
+                {
+
+                    int tmp = array[i];
+                    array[i++] = array[j];
+                    array[j--] = tmp;
+                }
+            }
+            if (left < j) QuickSort(array, left, j);
+            if (i < right) QuickSort(array, i, right);
+        }
+        public void HeapSort(int[] array, int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(array, n, i);
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = array[0];
+                array[0] = array[i];
+                array[i] = temp;
+                Heapify(array, i, 0);
+            }
+        }
+        private static void Heapify(int[] array, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            if (left < n && array[left] > array[largest])
+                largest = left;
+            if (right < n && array[right] > array[largest])
+                largest = right;
+            if (largest != i)
+            {
+                int swap = array[i];
+                array[i] = array[largest];
+                array[largest] = swap;
+                Heapify(array, n, largest);
+            }
+        }
+        public void ShellSort(int[] array, int n)
+        {
+            int i, j, pos, temp;
+            pos = 3;
+            while (pos > 0)
+            {
+                for (i = 0; i < n; i++)
+                {
+                    j = i;
+                    temp = array[i];
+                    while ((j >= pos) && (array[j - pos] > temp))
+                    {
+                        array[j] = array[j - pos];
+                        j = j - pos;
+                    }
+                    array[j] = temp;
+                }
+                if (pos / 2 != 0)
+                    pos = pos / 2;
+                else if (pos == 1)
+                    pos = 0;
+                else
+                    pos = 1;
+            }
+        }
+        public int[] MergeSort(int[] array)
+        {
+            int[] left;
+            int[] right;
+            int[] result = new int[array.Length];
+
+            if (array.Length <= 1)
+                return array;
+
+            int midPoint = array.Length / 2;
+            left = new int[midPoint];
+
+            if (array.Length % 2 == 0)
+                right = new int[midPoint];
+            else
+                right = new int[midPoint + 1];
+
+            for (int i = 0; i < midPoint; i++)
+                left[i] = array[i];
+
+            int x = 0;
+
+            for (int i = midPoint; i < array.Length; i++)
+            {
+                right[x] = array[i];
+                x++;
+            }
+            left = MergeSort(left);
+            right = MergeSort(right);
+
+            result = Merge(left, right);
+            return result;
+        }
+        private static int[] Merge(int[] left, int[] right)
+        {
+            int resultLength = right.Length + left.Length;
+            int[] result = new int[resultLength];
+            int indexLeft = 0, indexRight = 0, indexResult = 0;
+            while (indexLeft < left.Length || indexRight < right.Length)
+            {
+                if (indexLeft < left.Length && indexRight < right.Length)
+                {
+                    if (left[indexLeft] <= right[indexRight])
+                    {
+                        result[indexResult] = left[indexLeft];
+                        indexLeft++;
+                        indexResult++;
+                    }
+                    else
+                    {
+                        result[indexResult] = right[indexRight];
+                        indexRight++;
+                        indexResult++;
+                    }
+                }
+                else if (indexLeft < left.Length)
+                {
+                    result[indexResult] = left[indexLeft];
+                    indexLeft++;
+                    indexResult++;
+                }
+                else if (indexRight < right.Length)
+                {
+                    result[indexResult] = right[indexRight];
+                    indexRight++;
+                    indexResult++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Algorytmy_sortowania/Calosc/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calosc
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+          
+            SmallDataSort();
+            MiddleDataSort();
+            BigDataSort();
+            Console.ReadKey();
+
+
+        }
+        static void SmallDataSort()
+        {
+            Console.WriteLine("Small Data");
+            int size = 10;
+            Algorithms algorithm = new Algorithms();
+
+            int[] arr = new int[size];
+            ArrayData(arr, size);
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            algorithm.BubbleSort(arr);
+            stopwatch.Stop();
+            Console.WriteLine("Time of bubblesort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.QuickSort(arr,0,arr.Length-1);
+            stopwatch.Stop();
+            Console.WriteLine("Time of quicksort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.HeapSort(arr,  arr.Length );
+            stopwatch.Stop();
+            Console.WriteLine("Time of heapsort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.ShellSort(arr, arr.Length);
+            stopwatch.Stop();
+            Console.WriteLine("Time of shellsort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.MergeSort(arr);
+            stopwatch.Stop();
+            Console.WriteLine("Time of mergesort: " + stopwatch.Elapsed.ToString());
+
+        }
+        static void MiddleDataSort()
+        {
+            Console.WriteLine("Middle Data");
+            int size = 1000;
+            Algorithms algorithm = new Algorithms();
+            int[] arr = new int[size];
+            ArrayData(arr, size);
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            algorithm.BubbleSort(arr);
+            stopwatch.Stop();
+            Console.WriteLine("Time of bubblesort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.QuickSort(arr, 0, arr.Length - 1);
+            stopwatch.Stop();
+            Console.WriteLine("Time of quicksort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.HeapSort(arr, arr.Length);
+            stopwatch.Stop();
+            Console.WriteLine("Time of heapsort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.ShellSort(arr, arr.Length);
+            stopwatch.Stop();
+            Console.WriteLine("Time of shellsort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.MergeSort(arr);
+            stopwatch.Stop();
+            Console.WriteLine("Time of mergesort: " + stopwatch.Elapsed.ToString());
+
+        }
+        static void BigDataSort()
+        {
+            Console.WriteLine("Big Data");
+            int size = 100000;
+            Algorithms algorithm = new Algorithms();
+            int[] arr = new int[size];
+            ArrayData(arr, size);
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            algorithm.BubbleSort(arr);
+            stopwatch.Stop();
+            Console.WriteLine("Time of bubblesort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.QuickSort(arr, 0, arr.Length - 1);
+            stopwatch.Stop();
+            Console.WriteLine("Time of quicksort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.HeapSort(arr, arr.Length);
+            stopwatch.Stop();
+            Console.WriteLine("Time of heapsort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.ShellSort(arr, arr.Length);
+            stopwatch.Stop();
+            Console.WriteLine("Time of shellsort: " + stopwatch.Elapsed.ToString());
+
+            ArrayData(arr, size);
+            stopwatch = Stopwatch.StartNew();
+            algorithm.MergeSort(arr);
+            stopwatch.Stop();
+            Console.WriteLine("Time of mergesort: " + stopwatch.Elapsed.ToString());
+
+        }
+        static void ArrayData(int[] arr, int arrSize)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arrSize; i++) {
+                arr[i] = random.Next();
+            }
+        }
+        
+    }
+}
